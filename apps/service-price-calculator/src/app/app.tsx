07f78f38172de69761c5538f3b09a6f21308b9b8
Karol Binkowski <starystachu9@gmail.com>
2023-06-02 11:58:54 +0200

    Initial commit

---
+++ apps/service-price-calculator/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="service-price-calculator" />
+    </div>
+  );
+}
+
+export default App;
