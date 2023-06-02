b56594b4b07d984e3307892204c887ba066d3903
Karol Binkowski <starystachu9@gmail.com>
2023-06-02 19:36:01 +0200

    feat(service-price-calculator): add hero section component

---
+++ apps/service-price-calculator/src/components/hero/hero.tsx
@@ -0,0 +1,15 @@
+export function Hero() {
+  return (
+    <section className="text-center">
+      <hgroup>
+        <h1>Calculate your service pricing.</h1>
+        <h2>
+          This application will help you in choosing the right plan for your
+          services.
+        </h2>
+      </hgroup>
+    </section>
+  );
+}
+
+export default Hero;
