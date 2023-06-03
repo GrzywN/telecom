eeba614642dacaafd91fa8346407df6df6e068cd
Karol Binkowski <starystachu9@gmail.com>
2023-06-03 15:34:18 +0200

    test(service-price-calculator): provide context and browser router where needed

--- apps/service-price-calculator/src/features/calculator/components/calculator-router/calculator-router.spec.tsx
+++ apps/service-price-calculator/src/features/calculator/components/calculator-router/calculator-router.spec.tsx
@@ -1,10 +1,19 @@
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import CalculatorRouter from './calculator-router';
 
 describe('CalculatorRouter', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<CalculatorRouter />);
+    const { baseElement } = render(
+      <BrowserRouter>
+        <CalculatorRouter
+          firstStepComponent={null}
+          secondStepComponent={null}
+          secondStepPath={'/summary'}
+        />
+      </BrowserRouter>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
--- apps/service-price-calculator/src/features/calculator/components/form/form.spec.tsx
+++ apps/service-price-calculator/src/features/calculator/components/form/form.spec.tsx
@@ -1,10 +1,18 @@
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { CalculatorProvider } from '../../context/calculator-context';
 
 import Form from './form';
 
 describe('Form', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Form />);
+    const { baseElement } = render(
+      <CalculatorProvider>
+        <BrowserRouter>
+          <Form />
+        </BrowserRouter>
+      </CalculatorProvider>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
