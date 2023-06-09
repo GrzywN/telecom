9365310d18abf174705db66366adf188cdc88fc6
Karol Binkowski <starystachu9@gmail.com>
2023-06-09 16:02:48 +0200

    fix(calculator-core): fix json structure (2025 year)

--- libs/calculator/core/src/features/best-deal/utils/example-inputs/available-offers.ts
+++ libs/calculator/core/src/features/best-deal/utils/example-inputs/available-offers.ts
@@ -80,44 +80,44 @@ export const availableOffers: Offers = Object.freeze({
         },
       ],
     },
-  },
-  '2025': {
-    products: [
-      {
-        productId: 1,
-        name: 'Internet',
-        price: 59,
-      },
-      {
-        productId: 2,
-        name: 'Television',
-        price: 59,
-      },
-      {
-        productId: 3,
-        name: 'Telephone subscription',
-        price: 29,
-      },
-      {
-        productId: 4,
-        name: '4K decoder',
-        price: 29,
-      },
-    ],
-    bundles: [
-      {
-        bundleId: 1,
-        productIds: [1, 2],
-        name: 'Internet + Television',
-        price: 99,
-        extras: [4],
-      },
-      {
-        bundleId: 2,
-        productIds: [1, 3],
-        name: 'Internet + Telephone subscription',
-        price: 64,
-      },
-    ],
+    '2025': {
+      products: [
+        {
+          productId: 1,
+          name: 'Internet',
+          price: 59,
+        },
+        {
+          productId: 2,
+          name: 'Television',
+          price: 59,
+        },
+        {
+          productId: 3,
+          name: 'Telephone subscription',
+          price: 29,
+        },
+        {
+          productId: 4,
+          name: '4K decoder',
+          price: 29,
+        },
+      ],
+      bundles: [
+        {
+          bundleId: 1,
+          productIds: [1, 2],
+          name: 'Internet + Television',
+          price: 99,
+          extras: [4],
+        },
+        {
+          bundleId: 2,
+          productIds: [1, 3],
+          name: 'Internet + Telephone subscription',
+          price: 64,
+        },
+      ],
+    },
   },
 });
