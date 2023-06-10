525ce5ff740d547f1943025711fc7a893d8942b4
Karol Binkowski <starystachu9@gmail.com>
2023-06-10 15:45:16 +0200

    feat(service-price-calculator): add link for going back (from summary page)

--- apps/service-price-calculator/src/app/features/calculator/components/summary/summary.tsx
+++ apps/service-price-calculator/src/app/features/calculator/components/summary/summary.tsx
@@ -1,10 +1,16 @@
+import { Link } from 'react-router-dom';
+
 import { useCalculator } from '../../context/calculator-context';
+import styles from './summary.module.css';
 
 export function Summary() {
   const { isLoading, selectedYear, selectedServices } = useCalculator();
 
   return (
     <div aria-busy={isLoading}>
+      <Link to="/" className={styles['go-back-button']}>
+        ← Go Back
+      </Link>
       <p>You choosed year: {selectedYear}</p>
       <div>
         And services:
